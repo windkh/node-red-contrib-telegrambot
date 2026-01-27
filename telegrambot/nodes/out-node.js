39bc7db70fbb38084d5310ea805ac7ee2fb551c4
Marcus K <36983163+MK-2001@users.noreply.github.com>
2026-01-27 16:56:54 +0100

    Fix indentation and formatting in out-node.js

--- telegrambot/nodes/out-node.js
+++ telegrambot/nodes/out-node.js
@@ -7,8 +7,8 @@ JSON.safeStringify = (obj, indent = 2) => {
             typeof value === 'object' && value !== null
                 ? cache.includes(value)
                     ? undefined // Duplicate reference found, discard key
-                        : cache.push(value) && value // Store value in our collection
-                    : value,
+                    : cache.push(value) && value // Store value in our collection
+                : value,
         indent
     );
     cache = null;
